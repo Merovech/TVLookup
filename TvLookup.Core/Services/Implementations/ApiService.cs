b3755ed9dabb8651e9e1f3e0bdf525e487d0cebc
Ari Roth <ari.roth@gmail.com>
2022-06-13 23:26:02 -0700

    Implemented TVMaze API and set up initial project structure. (#1)

---
+++ TvLookup.Core/Services/Implementations/ApiService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TvLookup.Core.Models;
+using TvLookup.Core.Services.Interfaces;
+
+namespace TvLookup.Core.Services.Implementations
+{
+	/// <summary>
+	/// Implementation of portions of the TvMaze API
+	/// </summary>
+	// TODO: Logging
+	public class ApiService : IApiService
+	{
+		private static HttpClient Client = new();
+		private const string ROOT_URL = "https://api.tvmaze.com";
+
+		public async Task<List<TvShow>> FindShow(string searchString)
+		{
+			var apiPath = $"/search/shows?q={WebUtility.HtmlEncode(searchString)}";
+			var results = await ExecuteApi<List<TvShowSearchResult>>(apiPath);
+
+			// Result order is already by relevance, so no need to re-sort.  However, we don't need the score.
+			// Unsure if this is strictly necessary, but we're not dealing with a lot of data here so it can't
+			// really hurt.
+			var shows = results.Select(res => res.Show).ToList();
+
+			// TODO: Again, logging
+			foreach (var s in shows)
+			{
+				Debug.WriteLine(s.ToString());
+			}
+
+			return shows;
+		}
+
+		public async Task<List<TvShowEpisode>> GetEpisodes(int showId)
+		{
+			var apiPath = $"/shows/{showId}/episodes";
+			var results = await ExecuteApi<List<TvShowEpisode>>(apiPath);
+
+			// TODO: Again, logging
+			foreach (var r in results)
+			{
+				Debug.WriteLine(r.ToString());
+			}
+
+			return results;
+		}
+
+		private async Task<T> ExecuteApi<T>(string apiPath)
+		{
+			var response = (await Client.GetAsync($"{ROOT_URL}{apiPath}")).EnsureSuccessStatusCode();
+			var json = await response.Content.ReadAsStringAsync();
+			return JsonSerializer.Deserialize<T>(json);
+		}
+	}
+}
--- TvLookup.UI/MainWindow.xaml.cs
+++ TvLookup.UI/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using TvLookup.Core.Services.Implementations;
 
 namespace TvLookup.UI
 {
@@ -23,6 +12,21 @@ namespace TvLookup.UI
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		// Temporary, just to test out the API
+		private async void OnClick(object sender, RoutedEventArgs e)
+		{
+			ApiService svc = new ApiService();
+
+			// All of this is dumped into the debug window for the time being.  Until the Core functionality
+			// is fleshed out, the UI will serve only to allow me to do live tests.  Eventually those tests
+			// wil go into unit tests and this will turn into an MVVM WPF UI.
+			var shows = await svc.FindShow(this.tmpInput.Text);
+			if (shows.Any())
+			{
+				var episodes = await svc.GetEpisodes(shows.First().Id);
+			}
 		}
 	}
 }
